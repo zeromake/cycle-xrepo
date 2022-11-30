397e724a8ff8356346e1fd2559ab909fa08be5cd
zeromake <a390720046@gmail.com>
2022-11-30 15:44:54 +0800

    fix: add printf

--- packages/f/freetype/port/ft.c
+++ packages/f/freetype/port/ft.c
@@ -1,10 +1,12 @@
 #include "ft.h"
+#include <stdio.h>
 
 #ifdef FT_CONFIG_OPTION_USE_HARFBUZZ
 #include <hb.h>
 #endif
 
 void ft_font_create() {
+    printf("ft_font_create call\n");
 #ifdef FT_CONFIG_OPTION_USE_HARFBUZZ
     hb_ft_font_create();
 #endif
--- packages/h/harfbuzz/port/hb.c
+++ packages/h/harfbuzz/port/hb.c
@@ -1,15 +1,16 @@
 #include "hb.h"
+#include <stdio.h>
 
 #ifdef HAVE_FREETYPE
 #include <ft.h>
 #endif
 
 void hb_font_create() {
-
+    printf("hb_font_create call\n");
 }
 
 #ifdef HAVE_FREETYPE
 void hb_ft_font_create() {
-
+    printf("hb_ft_font_create call\n");
 }
 #endif
